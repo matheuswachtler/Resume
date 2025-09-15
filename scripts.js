061baea8e0799e60c329975ababea5df000a4736
matheuswachtler <matheus.wachtler@outlook.com>
2025-09-15 12:00:08 -0300

    Correção script.js para redirecionamento.

--- scripts.js
+++ scripts.js
@@ -11,9 +11,14 @@ function setLanguage(language) {
     if (language === 'pt') {
         if (window.location.pathname.endsWith('index.html') || window.location.pathname.endsWith('indexptbr.html')) {
             window.location.href = 'indexptbr.html';
+        } else if (window.location.pathname.endsWith('cv.html') || window.location.pathname.endsWith('cvptbr.html')) {
+            window.location.href = 'cvptbr.html';
         }
     } else if (language === 'en') {
-        if (window.location.pathname.endsWith('index.html')) {
+        if (window.location.pathname.endsWith('indexptbr.html') || window.location.pathname.endsWith('index.html')) {
+            window.location.href = 'index.html';
+        } else if (window.location.pathname.endsWith('cvptbr.html') || window.location.pathname.endsWith('cv.html')) {
+            window.location.href = 'cv.html';
         }
     }
     toggleLanguageMenu();
