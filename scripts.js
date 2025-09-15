73e84483e66bcb1ae6cf5b2f928ed1b8766fa391
matheuswachtler <matheus.wachtler@outlook.com>
2025-09-15 10:32:17 -0300

    Correção script.js

--- scripts.js
+++ scripts.js
@@ -11,16 +11,9 @@ function setLanguage(language) {
     if (language === 'pt') {
         if (window.location.pathname.endsWith('index.html') || window.location.pathname.endsWith('indexptbr.html')) {
             window.location.href = 'indexptbr.html';
-        } else if (window.location.pathname.endsWith('cv.html') || window.location.pathname.endsWith('cvptbr.html')) {
-            window.location.href = 'cvptbr.html';
         }
     } else if (language === 'en') {
-        if (window.location.pathname.endsWith('cvptbr.html') || window.location.pathname.endsWith('cv.html')) {
-            window.location.href = 'cv.html';
-        } else if (window.location.pathname.endsWith('indexptbr.html') || window.location.pathname.endsWith('index.html')) {
-            window.location.href = 'index.html';
-        } else {
-            alert('Idioma selecionado: ' + language);
+        if (window.location.pathname.endsWith('index.html')) {
         }
     }
     toggleLanguageMenu();
